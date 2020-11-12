386ff6e1db5dd3f9e256f9702b18e00b70566807
Karthikeyan Subramanian <skarthik.chennai@gmail.com>
2020-11-12 18:09:15 +0530

    Setting up backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const { resolveNaptr } = require("dns");
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, () => {
+  console.log("App running on port 5000");
+});

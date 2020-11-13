71808166e5425fa9829d66c37da3ed48198d076a
Karthikeyan Subramanian <skarthik.chennai@gmail.com>
2020-11-13 19:49:32 +0530

    Backend Model creation complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
-const { resolveNaptr } = require("dns");
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -17,6 +23,10 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, () => {
-  console.log("App running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
 });
